c23f267e6a2cac87c767fd7476db357df926ad66
Björn <bjorntaks@hotmail.com>
2018-03-27 13:25:11 +0200

    added tag

--- app/src/main/java/com/example/stijn/sath/MainActivity.java
+++ app/src/main/java/com/example/stijn/sath/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Film> films = new ArrayList<>();
+    private final static String TAG = MainActivity.class.getSimpleName();
 
 
     @Override
