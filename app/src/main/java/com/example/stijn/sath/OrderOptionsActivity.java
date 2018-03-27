352f67f71b8cc237687e42c92ee27ad39e39876b
Kelly Zegelaar <knzegela@avans.nl>
2018-03-27 21:32:07 +0200

    added OrderOptionsActivity

--- app/src/main/java/com/example/stijn/sath/OrderOptionsActivity.java
+++ app/src/main/java/com/example/stijn/sath/OrderOptionsActivity.java
@@ -3,44 +3,15 @@ package com.example.stijn.sath;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-public class FilmDetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class OrderOptionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_film_detail);
-
-        Bundle extras = getIntent().getExtras();
-
-        ImageView filmPoster = findViewById(R.id.filmPoster);
-        TextView filmTitle = findViewById(R.id.filmTitle);
-        TextView description = findViewById(R.id.filmDescription);
-        Button orderTickets = findViewById(R.id.btnOrder);
-
-        String imageURL = extras.getString(MainActivity.IMAGEURL);
-        String filmName = extras.getString(MainActivity.FILMNAME);
-        String filmDescription = extras.getString(MainActivity.FILMDESCRIPTION);
-
-        description.setText(filmDescription);
-        filmTitle.setText(filmName);
-        Picasso.with(getApplicationContext()).load(imageURL).into(filmPoster);
-        orderTickets.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent i = new Intent(getApplicationContext(), OrderOptionsActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_order_options);
     }
 
     @Override
