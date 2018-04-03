f8a1b30d401f48d01f92292581227aea0e21f743
Stijn van Veen <stijnboz@live.nl>
2018-04-03 13:44:46 +0200

    Added button functionalitality

--- app/src/main/java/com/example/stijn/sath/gui/PaymentConfirmActivity.java
+++ app/src/main/java/com/example/stijn/sath/gui/PaymentConfirmActivity.java
@@ -1,11 +1,16 @@
 package com.example.stijn.sath.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.stijn.sath.R;
+import com.example.stijn.sath.domain.Seat;
+
+import java.util.ArrayList;
 
 /**
  * Created by btaks on 3-4-2018.
@@ -20,5 +25,13 @@ public class PaymentConfirmActivity extends AppCompatActivity {
 
         TextView text = (TextView) findViewById(R.id.a_payment_done_text);
         Button button = (Button) findViewById(R.id.a_payment_done_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Intent i = new Intent(getApplicationContext(), MainActivity.class);
+             startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/stijn/sath/gui/PaymentFailActivity.java
+++ app/src/main/java/com/example/stijn/sath/gui/PaymentFailActivity.java
@@ -1,7 +1,9 @@
 package com.example.stijn.sath.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,5 +22,13 @@ public class PaymentFailActivity extends AppCompatActivity {
 
         TextView text = (TextView) findViewById(R.id.a_payment_fail_text);
         Button button = (Button) findViewById(R.id.a_payment_fail_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PaymentActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
